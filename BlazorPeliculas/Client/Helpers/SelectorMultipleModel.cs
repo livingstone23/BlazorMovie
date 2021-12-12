75569e3024f7dacac1fbcb78aefb603a571f374c
LCANOASUS <livingstone23@gmail.com>
2021-12-12 12:05:13 +0100

    Creados componentes, para proceso de comunicar con BackEnd

---
+++ BlazorPeliculas/Client/Helpers/SelectorMultipleModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorPeliculas.Client.Helpers
+{
+    public struct SelectorMultipleModel
+    {
+        public SelectorMultipleModel(string llave, string valor)
+        {
+            Llave = llave;
+            Valor = valor;
+        }
+
+        public string Llave { get; set; }
+        public string Valor { get; set; }
+    }
+}
--- BlazorPeliculas/Shared/Entidades/Pelicula.cs
+++ BlazorPeliculas/Shared/Entidades/Pelicula.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace BlazorPeliculas.Shared.Entidades
 {
     public class Pelicula
     {
+        public int Id { get; set; }
+        [Required]
         public string Titulo { get; set; }
-        public DateTime Lanzamiento { get; set; }
-
+        public string Resumen { get; set; }
+        public bool EnCartelera { get; set; }
+        public string Trailer { get; set; }
+        [Required]
+        public DateTime? Lanzamiento { get; set; }
         public string Poster { get; set; }
-
-        public string TituloCorto
+        public List<GeneroPelicula> GenerosPelicula { get; set; } = new List<GeneroPelicula>();
+        public string TituloCortado
         {
             get
             {
